82cc748cbad915fe158347fbe67dbc179d60c18f
Achim Königs <garfonso@tratschtante.de>
2014-05-15 16:05:02 +0200

    added helper to avoid try/catch blocks
    it seems try/catch blocks are slow in V8 (i.e. node.js).
    I added them just to prevent futures from throwing exceptions
    at me. This works fine, too.
    
    Bonus: Made error handling a bit simpler in some places.

--- service/javascript/assistants/checkUpdateAssistant.js
+++ service/javascript/assistants/checkUpdateAssistant.js
@@ -20,69 +20,65 @@ CheckUpdateAssistant.prototype.run = function (outerFuture) {
 	future.nest(Utils.getLocalPlatformVersion());
 
 	future.then(this, function localVersionCallback() {
-		try {
-			var result = future.result;
-			log("localVersion came back: " + JSON.stringify(result));
-			if (result.returnValue === true) {
-				localVersion = result.version;
-				future.nest(Utils.getManifest());
-			} else {
-				throw {message: "Could not get local plattform version. No error specified.", errorCode: -1};
-			}
-		} catch (e) {
-			log("localVersion came back WITH ERROR: " + JSON.stringify(e));
-			handleError("Could not get local plattform version", e);
-		}
+        var result = Utils.checkResult(future);
+        log("localVersion came back: " + JSON.stringify(result));
+        if (result.returnValue === true) {
+            localVersion = result.version;
+            future.nest(Utils.getManifest());
+        } else {
+            log("localVersion came back WITH ERROR: " + JSON.stringify(result));
+            handleError("Could not get local plattform version.", result.exception);
+        }
 	});
 
 	future.then(this, function manifestCallback() {
-		try {
-			var result = future.result, changesSinceLast = [], newResult;
-			if (result && result.returnValue === true) {
-				manifest = result.manifest;
-				remoteVersion = manifest.platformVersion;
+        var result = Utils.checkResult(future), changesSinceLast = [], newResult;
+        if (result && result.returnValue === true) {
+            manifest = result.manifest;
+            remoteVersion = manifest.platformVersion;
 
-				if (!remoteVersion) {
-					handleError("Could not parse remote version from manifest", {message: JSON.stringify(manifest)});
-					return;
-				}
+            if (!remoteVersion) {
+                handleError("Could not parse remote version from manifest", {message: JSON.stringify(manifest)});
+                return;
+            }
 
-                log("Remote version came back: " + remoteVersion);
-				if (remoteVersion > localVersion) {
-					//get changes since last update:
-					manifest.changeLog.forEach(function filterChanges(change) {
-						if (change.version > localVersion) {
-							changesSinceLast.push(change);
-						}
-					});
-					changesSinceLast.sort(function sortFunction(a, b) {
-						return b.version - a.version;
-					});
+            log("Remote version came back: " + remoteVersion);
+            if (remoteVersion > localVersion) {
+                //get changes since last update:
+                manifest.changeLog.forEach(function filterChanges(change) {
+                    if (change.version > localVersion) {
+                        changesSinceLast.push(change);
+                    }
+                });
+                changesSinceLast.sort(function sortFunction(a, b) {
+                    return b.version - a.version;
+                });
 
-					newResult = { returnValue: true, success: true, needUpdate: true,
-									changesSinceLast: changesSinceLast };
+                newResult = {
+                    returnValue: true,
+                    success: true,
+                    needUpdate: true,
+                    changesSinceLast: changesSinceLast
+                };
 
-					//notify user that we have an update:
-					//TODO: replace with something else that creates a user notification.
-					/*PalmCall.call("palm://com.palm.applicationManager", "launch", {
-						id: "org.webosports.app.update",
-						params: newResult
-					}).then(function appManagerCallback(f) {
-						log("ApplicationManager call came back: " + JSON.stringify(f.result));
-					});*/
+                //notify user that we have an update:
+                //TODO: replace with something else that creates a user notification.
+                /*PalmCall.call("palm://com.palm.applicationManager", "launch", {
+                    id: "org.webosports.app.update",
+                    params: newResult
+                }).then(function appManagerCallback(f) {
+                    log("ApplicationManager call came back: " + JSON.stringify(f.result));
+                });*/
 
-					outerFuture.result = newResult;
-				} else {
-                    //no update necessary.
-                    outerFuture.result = { returnValue: true, success: true,  needUpdate: false};
-                }
+                outerFuture.result = newResult;
+            } else {
+                //no update necessary.
+                outerFuture.result = { returnValue: true, success: true,  needUpdate: false};
+            }
 
-			} else {
-				handleError("Could not get manifest", future.exception);
-			}
-		} catch (e) {
-			handleError("Could not get manifest", e);
-		}
+        } else {
+            handleError("Could not get manifest", future.exception);
+        }
 	});
 
 	return outerFuture;
--- service/javascript/assistants/downloadUpdateAssistant.js
+++ service/javascript/assistants/downloadUpdateAssistant.js
@@ -43,55 +43,47 @@ DownloadUpdateAssistant.prototype.run = function (outerFuture, subscription) {
 
 	//handles child process output and termination:
 	function childCallback() {
-		try {
-			var result = future.result;
-			if (result.finished && result.error === false) {
-				if (doneGetNumPackages) {
-					log("Download Log:\n" + Parser.getDownloadLog());
-					//we are done:
-					outerFuture.result = {success: true, finished: true, error: false, msg: "Done downloading."};
-				} else if (doneUpdating) {
-					doneGetNumPackages = true;
-					toDownload = Parser.getNumPackages();
-					log("Get num packages: " + toDownload);
-					future.nest(Utils.spawnChild(Config.downloadCommand, Parser.parseDownloadOutput.bind({}, logToApp)));
-					future.then(childCallback);
-				} else {
-					//package feed update finished. Go on.
-					doneUpdating = true;
-					log("Update Log:\n" + Parser.getUpdateLog());
+        var result = Utils.checkResult(future);
+        if (result.finished && result.error === false) {
+            if (doneGetNumPackages) {
+                log("Download Log:\n" + Parser.getDownloadLog());
+                //we are done:
+                outerFuture.result = {success: true, finished: true, error: false, msg: "Done downloading."};
+            } else if (doneUpdating) {
+                doneGetNumPackages = true;
+                toDownload = Parser.getNumPackages();
+                log("Get num packages: " + toDownload);
+                future.nest(Utils.spawnChild(Config.downloadCommand, Parser.parseDownloadOutput.bind({}, logToApp)));
+                future.then(childCallback);
+            } else {
+                //package feed update finished. Go on.
+                doneUpdating = true;
+                log("Update Log:\n" + Parser.getUpdateLog());
 
-					future.nest(Utils.spawnChild(Config.numPackagesCommand, Parser.parseNumPackages));
-					future.then(childCallback);
-				}
-			} else {
-				throw ({message: Parser.getErrorMessage() || "Child did finish with error", errorCode: result.code});
-			}
-		} catch (e) {
-			handleError("Error during " + (doneUpdating ? "downloading packages" : "updating feeds"), e);
-		}
+                future.nest(Utils.spawnChild(Config.numPackagesCommand, Parser.parseNumPackages));
+                future.then(childCallback);
+            }
+        } else {
+            handleError("Error during " + (doneUpdating ? "downloading packages" : "updating feeds"), result.exception || Parser.getErrorMessage());
+        }
 	}
-
+    
 	Parser.clear();
 
 	future.nest(Utils.checkDirectory(Config.downloadPath));
 
 	future.then(function pathCB() {
-		try {
-			var result = future.result;
-			if (result.returnValue) {
-				if (args.skipFeedsUpdate) {
-					doneUpdating = true;
-					future.nest(Utils.spawnChild(Config.numPackagesCommand, Parser.parseNumPackages));
-				} else {
-					future.nest(Utils.spawnChild(Config.preDownloadCommand, Parser.parseUpdateOutput));
-				}
-			} else {
-				throw {message: "Unknown error: " + JSON.stringify(result)};
-			}
-		} catch (e) {
-			handleError("Error during checking/creating download directory", e);
-		}
+        var result = Utils.checkResult(future);
+        if (result.returnValue) {
+            if (args.skipFeedsUpdate) {
+                doneUpdating = true;
+                future.nest(Utils.spawnChild(Config.numPackagesCommand, Parser.parseNumPackages));
+            } else {
+                future.nest(Utils.spawnChild(Config.preDownloadCommand, Parser.parseUpdateOutput));
+            }
+        } else {
+            handleError("Error during checking/creating download directory", result.exception);
+        }
 	});
 
 	future.then(childCallback);
